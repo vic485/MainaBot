c4f5d48434cb211cc848287ea422ec2ee3f5aded
Victor Fugazzotto <victorfuga@gmail.com>
2020-02-18 20:17:48 -0500

    Adjusted embed colors

--- Maina/Core/EmbedColor.cs
+++ Maina/Core/EmbedColor.cs
@@ -5,11 +5,13 @@
 	public enum EmbedColor
 	{
 		SalmonPink = 0xFDA99E,    // Maina's salmon pink.
-		Green = 0x006D38,   // Sanae's eyes
-		Purple = 0xAC82DC,  // Reisen's hair
-		Red = 0xFF462C,    // Reimu's outfit
-		Yellow = 0xFFF103,   // Marisa's hair
+		Green = 0x73CAAD,   // Aya's green
+		Purple = 0xB5B4E8,  // Yumeri's purple
+		Red = 0xBA4E72,    // Eripiyo's tracksuit (kinda red?)
+		Yellow = 0xF3E0A3,   // Maki's yellow
 		White = 0xFFFFFF,   //Just white
-
+		Pink = 0xFFB8DC, // Reo's pink
+		Blue = 0x77CFEF, // Sorane's blue
+		LightBlue = 0xD4ECF4 // Yuuka's light blue
 	}
 }
