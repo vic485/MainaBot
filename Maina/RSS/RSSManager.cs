f37206394753d05caff7fd7c8c2f3be81f2f2956
Enrique Ávila Rodríguez <enrique.avila.y@gmail.com>
2020-02-20 21:59:55 +0100

    Fixed RSS news not displaying covers properly when format wasn't .jpeg

--- Maina/RSS/RSSManager.cs
+++ Maina/RSS/RSSManager.cs
@@ -2,8 +2,10 @@
 using Discord.WebSocket;
 using Maina.Administrative;
 using Maina.Core;
+using Maina.Core.Logging;
 using Maina.Database;
 using System;
+using System.Net;
 
 namespace Maina.RSS
 {
@@ -30,20 +32,44 @@ namespace Maina.RSS
 
 		}
 
-		
 
+		
+		
 		private async void OnRSSUpdateAsync(object sender, RSSUpdateEventArgs e)
 		{
 			EmbedBuilder eb = new EmbedBuilder { Color = new Color((uint) EmbedColor.SalmonPink) };
 			eb.WithAuthor("I've heard some great news!");
-			int idIndex = e.Feed.Id.LastIndexOf("/") +1;
-			int id = -1;
-			if (int.TryParse(e.Feed.Id.Substring(idIndex), out id))
-				eb.WithImageUrl($"https://mangadex.org/images/manga/{id}.jpeg");
 			eb.WithTitle(e.Update.Title.Text);
 			eb.WithUrl(e.Update.Id);
 			eb.WithFooter($"There is a new chapter available to read!");
+
+			int idIndex = e.Feed.Id.LastIndexOf("/") +1;
+			int id = -1;
+			string imageUrl = null;
+			if (int.TryParse(e.Feed.Id.Substring(idIndex), out id))
+				imageUrl = $"https://mangadex.org/images/manga/{id}.";
+			if (imageUrl != null) {
+				if (DoesImageExist(ref imageUrl))
+					eb.WithImageUrl(imageUrl);
+			}
+
 			await DiscordAPIHelper.PublishNews(eb, _databaseManager, _discordSocketClient, e.Feed.Tag);
+		}
+
+		private static readonly string [] _COVER_EXTENSIONS = { "jpg", "jpeg", "png" };
+		private bool DoesImageExist (ref string baseUrl) {
+			bool exists = false;
+			for (int i = 0; i < _COVER_EXTENSIONS.Length && !exists; i++) {
+				try {
+					HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseUrl + _COVER_EXTENSIONS[i]);
+					using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+						if (exists = (response.StatusCode == HttpStatusCode.OK))
+							baseUrl = baseUrl + _COVER_EXTENSIONS[i];
+					}
+				}
+				catch (Exception) {}
+			}
+			return exists;
 		}
 
 
